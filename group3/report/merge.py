67de0f5f905d14a3c668aff8ea1c64bc5f049757
huyenuet <thanhhuyenk59uet@gmail.com>
2017-12-01 12:50:43 +0700

    edit report

--- group3/report/merge.py
+++ group3/report/merge.py
@@ -3,17 +3,17 @@ import codecs, os
 # reportFolderPath = '/Volumes/DATA/STUDY/Vandehiendai/git/new/int3507-2017/group3/report'
 # reportFolderPath = 'D:\uet\van_de_hien_dai\int3507-2017\group3\report'
 
-filenames = ['1. Thông tin nhóm.md',
-             '2. Đặt vấn đề.md',
-             '3.1. Kiến trúc scrapy.md',
-             '3.2. Cài đặt scrapy.md',
-             '3.3. Spider.md',
-             '3.4. Extractor.md',
-             '3.5. Pipeline.md',
-             '3.6. Lưu dữ liệu.md',
-             '3.7. Cấu hình nâng cao Scrapy.md',
-             '3.8. Các vấn đề cần giải quyết với Scrapy.md',
-             '4. Kết luận.md']
+filenames = ['0. Thông tin nhóm.md',
+             '1. Đặt vấn đề.md',
+             '2.1. Kiến trúc scrapy.md',
+             '2.2. Cài đặt scrapy.md',
+             '2.3. Spider.md',
+             '2.4. Trích xuất dữ liệu.md',
+             '2.5. Item Pipeline.md',
+             '2.6. Lưu dữ liệu.md',
+             '2.7. Cấu hình nâng cao Scrapy.md',
+             '2.8. Các vấn đề cần giải quyết với Scrapy.md',
+             '3. Kết luận.md']
 with open('../README.md', 'w') as outfile:
     for fname in filenames:
         with open(fname) as infile:
